33f717a454523182f02803c65a9252196dbcb369
Jelle Siderius <jsiderius1992@gmail.com>
2024-05-17 11:51:56 +0200

    Update pickup-shop.js
    Added translation through mage/translate

--- view/frontend/web/js/view/checkout/shipping-information/pickup-shop.js
+++ view/frontend/web/js/view/checkout/shipping-information/pickup-shop.js
@@ -1,12 +1,14 @@
 define(
     [
-    'jquery',
-    'uiComponent',
-    'ko'
+        'jquery',
+        'uiComponent',
+        'ko',
+        'mage/translate'
     ], function (
         $,
         Component,
-        ko
+        ko,
+        $t
     ) {
         return Component.extend(
             {
@@ -34,57 +36,19 @@ define(
                         var delivery_options = montapacking.details[0].options;
 
                         var additional_info = [];
-                        if ($("#montapacking_language").val() == 'NL' || $("#montapacking_language").val() == 'BE') {
-
-                            $(delivery_options).each(
-                                function (key, value) {
-
-                                    if (value == 'SignatureOnDelivery') {
-                                        additional_info.push("Tekenen bij ontvangst");
-                                    } else if (value == 'NoNeighbour') {
-                                        additional_info.push("Niet bij de buren afleveren");
-                                    } else if (value == 'EveningDelivery') {
-                                        additional_info.push("In avond afleveren");
-                                    } else {
-                                        additional_info.push(value);
-                                    }
-
+                        $(delivery_options).each(
+                            function (key, value) {
+                                if (value == 'SignatureOnDelivery') {
+                                    additional_info.push($t("Signature on delivery"));
+                                } else if (value == 'NoNeighbour') {
+                                    additional_info.push($t("No delivery at neighbour"));
+                                } else if (value == 'EveningDelivery') {
+                                    additional_info.push($t("Evening delivery"));
+                                } else {
+                                    additional_info.push(value);
                                 }
-                            );
-                        } else if ($("#montapacking_language").val() == 'DE') {
-
-                            $(delivery_options).each(
-                                function (key, value) {
-
-                                    if (value == 'SignatureOnDelivery') {
-                                        additional_info.push("Auf Quittung unterschreiben");
-                                    } else if (value == 'NoNeighbour') {
-                                        additional_info.push("Nicht an die Nachbarn liefern");
-                                    } else if (value == 'EveningDelivery') {
-                                        additional_info.push("Lieferung am Abend");
-                                    } else {
-                                        additional_info.push(value);
-                                    }
-
-                                }
-                            );
-                        } else {
-                            $(delivery_options).each(
-                                function (key, value) {
-
-                                    if (value == 'SignatureOnDelivery') {
-                                        additional_info.push("Signature on delivery");
-                                    } else if (value == 'NoNeighbour') {
-                                        additional_info.push("No delivery at neighbour");
-                                    } else if (value == 'EveningDelivery') {
-                                        additional_info.push("Evening delivery");
-                                    } else {
-                                        additional_info.push(value);
-                                    }
-
-                                }
-                            );
-                        }
+                            }
+                        );
 
 
                         this.deliveryOptions(additional_info);
