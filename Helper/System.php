66889744deccdeb2bf2a45ec1f9978f6e79962cd
Jacco Amersfoort <jacco.amersfoort@monta.nl>
2025-05-14 11:08:42 +0200

    #19109 new System helper for system diagnostics

---
+++ Helper/System.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * @author Jacco.Amersfoort <jacco.amersfoort@monta.nl>
+ * @created 5/14/2025 10:59
+ */
+namespace Montapacking\MontaCheckout\Helper;
+
+class System
+{
+    public function __construct()
+    {
+    }
+
+    public function getDiagnostics()
+    {
+        return [
+            'test' => "hello",
+        ];
+    }
+
+}
