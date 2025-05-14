70a810656a9cf39002b89f57a54b0c33653310cf
Jacco Amersfoort <jacco.amersfoort@monta.nl>
2025-05-14 12:07:44 +0200

    #19109 Add module version information
    Add Magento name and modules versions to output, plus some system data

--- Helper/System.php
+++ Helper/System.php
@@ -5,17 +5,35 @@
  */
 namespace Montapacking\MontaCheckout\Helper;
 
-class System
+use Composer\InstalledVersions;
+use Magento\Framework\App\Helper\AbstractHelper;
+use Magento\Framework\Module\ResourceInterface;
+
+class System extends AbstractHelper
 {
-    public function __construct()
+    /**
+     * @param ResourceInterface $resource
+     */
+    public function __construct(
+        protected readonly ResourceInterface $resource)
     {
     }
 
+    /** Collect system information
+     *
+     * @return string[]
+     */
     public function getDiagnostics()
     {
+        $moduleName = $this->_getModuleName();
         return [
-            'test' => "hello",
+            'core_software' => 'Magento',
+            'core_version' => InstalledVersions::getVersion('magento/product-community-edition'),
+            'checkout_api_wrapper_version' => InstalledVersions::getVersion('monta/checkout-api-wrapper'),
+            'module_name' => $moduleName,
+            'module_version' => $this->resource->getDbVersion($moduleName),
+            'php_version' => PHP_VERSION,
+            'operating_system' => PHP_OS,
         ];
     }
-
 }
